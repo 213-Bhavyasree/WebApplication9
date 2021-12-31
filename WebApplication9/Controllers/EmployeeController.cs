0a7f52d18c7e41568fb62d2a712e6a6fd76abcb8
Admin <kloklokesh846@gmail.com>
2021-12-31 17:26:29 +0530

    Add project files.

---
+++ WebApplication9/Controllers/EmployeeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication9.Models;
+
+namespace WebApplication9.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            IList<Employee> employees = new List<Employee>();
+            employees.Add(new Employee() { ID = 414, FirstName = "Bhavya", LastName = "Sree" });
+            employees.Add(new Employee() { ID = 415, FirstName = "Kavya", LastName = "Sree" });
+            employees.Add(new Employee() { ID = 416, FirstName = "Navya", LastName = "Sree" });
+
+            ViewData["employees"] = employees;
+
+            ViewBag.TotalEmployees = employees.Count();
+            ViewBag.CompanyName = "IBM";
+
+            return View();
+        }
+    }
+}
+
+
+
+
+
---
+++ WebApplication9/Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication9.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
+
+  
+
